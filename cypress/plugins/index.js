8b76d290fdb42edf2134011a1c539c39cc8f449e
seba53b4 <seba53b4@hotmail.com>
2021-11-12 01:57:01 -0300

    Added Allure Reports

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,6 +15,14 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+/// <reference types="@shelex/cypress-allure-plugin" />
+
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+
+module.exports = (on, config) => {
+  allureWriter(on, config);
+  return config;
+};
 
 
 // eslint-disable-next-line no-unused-vars
